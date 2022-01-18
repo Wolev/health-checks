ba818b07bf1a9d994b77c5bbcab7d740a9f38b2b
My_name <me@example.com>
2022-01-18 17:02:20 +0200

    Fixed repetition in the calling of function check_disk_full() in all_checks.py

--- all_checks.py
+++ all_checks.py
@@ -8,14 +8,14 @@ def check_reboot():
     # Check if the path "/run/reboot-required" exists
     return os.path.exists("/run/reboot-required")
 
-def check_disk_full(disk, min_absolute, min_percent):
-    """Returns True if there isn't enough disk space, False otherwise. """
+def check_disk_full(disk, min_gb, min_percent):
+    """Returns True if there isn't enough disk space, False otherwise."""
     du = shutil.disk_usage(disk)
     # Calculate the percentage of free space
     percent_free = 100 * du.free / du.total
     # Calculate how many free gigabytes
     gigabytes_free = du.free / 2**30
-    if percent_free < min_percent or gigabytes_free < min_absolute:
+    if percent_free < min_percent or gigabytes_free < min_gb:
         return True
     return False
 
@@ -23,10 +23,7 @@ def main():
     if check_reboot():
         print("Pending Reboot.")
         sys.exit(1) # The system is rebooting, EXIT_FAILUIRE
-    if check_disk_full("/", 2, 10):
-        print("Disk full.")
-        sys.exit(1)
-    if disk_full():
+    if check_disk_full(disk="/", min_gb=2, min_percent=10):
         print("Disk full.")
         sys.exit(1)
 
